db651957ed1265f88fb20fd88568cb8d3d59872d
wizo06 <14183421+wizo06@users.noreply.github.com>
2021-05-10 12:30:52 -0400

    Formatted y-axis to be human_format

--- src/helpers/generateTable.py
+++ src/helpers/generateTable.py
@@ -4,15 +4,29 @@ import pandas as pd
 from collections import Counter
 import datetime
 import sys
+import numpy as np
 
 df = pd.read_csv('data.csv').values
 dates = [datetime.date.fromisoformat(x).strftime(('%b%d')) for x in df[:, 0]]
 prices = df[:,1]
 
+def human_format(num, pos):
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num /= 1000.0
+    # add more suffixes if you need them
+    return '%.2f%s' % (num, ['', 'K', 'M', 'G', 'T', 'P'][magnitude])
+
+formatter = FuncFormatter(human_format)
+
+fig, ax = plt.subplots()
+ax.yaxis.set_major_formatter(formatter)
+
 plt.title('{} ~ {}'.format(df[0,0], df[-1,0]))
 plt.xticks(ticks=range(len(dates)), labels=dates, rotation=45, ha='right')
-plt.ticklabel_format(axis='y', style='plain')
 plt.ylabel('Price')
+plt.xlabel('Timeline')
 
 # Plot the mean line
 x1, y1 = [0, len(dates)], [prices.mean(), prices.mean()]
